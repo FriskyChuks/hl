39cee6a1d200fffff589df21fd5ddcb6b9bca1d8
FriskyChuks <chuksedwin.ce@gmail.com>
2021-09-02 16:08:58 +0100

    procfile, runtime

--- core/settings/production.py
+++ core/settings/production.py
@@ -18,8 +18,9 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
+
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['hlriders.herokuapp.com']
 
 
 AUTH_USER_MODEL = 'accounts.User'
