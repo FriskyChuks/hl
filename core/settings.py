9fd17176551b522bb25681cab8a6903fb729c62d
FriskyChuks <chuksedwin.ce@gmail.com>
2021-09-02 09:01:40 +0100

    first commit

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ from unipath import Path
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).parent
+BASE_DIR = Path(__file__).parent.parent
 CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
